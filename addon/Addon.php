741f742951b0685edb36fb0b26ddda9e3b9d629f
Alejandro Mostajo <amostajo@gmail.com>
2018-02-08 21:39:24 -0600

    Initial structure.

---
+++ addon/Addon.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AddonNamespace;
+
+use WPMVC\Addon;
+
+/**
+ * Addon class.
+ * Wordpress MVC.
+ *
+ * @link http://www.wordpress-mvc.com/v1/add-ons/
+ * @author [fill]
+ * @version [fill]
+ */
+class Addon extends Addon
+{
+    /**
+     * Function called when plugin or theme starts.
+     * Add wordpress hooks (actions, filters) here.
+     */
+    public function init()
+    {
+        // YOUR CUSTOM CODE HERE.
+    }
+    /**
+     * Function called when user is on admin dashboard.
+     * Add wordpress hooks (actions, filters) here.
+     */
+    public function on_admin()
+    {
+        // YOUR CUSTOM CODE HERE.
+    }
+}
